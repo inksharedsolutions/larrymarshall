bce5e15a07d5d63b611252ad66dd43c7349ef50a
inksharedsolutions <webdev.inkshared@gmail.com>
2020-09-16 23:35:33 +0800

    changes-9-16-2020

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Chair from '../../static/img/bench_chair.png'
+import Road from '../../static/img/road.png'
 
 const Quote = () =>{
     return(
         <>
             <section className="two-grid-elements" id="parent-quote-col">
-                <div className="grid-child-elements" id="left-col">
-                    <span></span>
-                    <span></span>
-                    <img src={Chair}/>
-                </div>
-                
                 <div className="grid-child-elements" >
                     <div className="text-content-wrapper">
                         <h1>On this day</h1>
@@ -18,7 +12,12 @@ const Quote = () =>{
                         <p>was compelling like hearing the whisper of an Epiphany that waits for you just around the corner.</p>
                         <span>Lawrence Gabriel.</span>
                     </div>
-                </div>          
+                </div>
+
+                <div className="grid-child-elements" id="left-col">
+                    <span></span>
+                    <span></span>
+                </div>
             </section>
         </>
     )
